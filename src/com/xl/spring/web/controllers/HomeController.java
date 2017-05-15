1a83b90b0d7a79a9609401c0c62b00db88457ba7
XiaoLong Chen <xiaolong.chen01@sap.com>
2017-05-16 02:30:57 +0800

    add log4j

--- src/com/xl/spring/web/controllers/HomeController.java
+++ src/com/xl/spring/web/controllers/HomeController.java
@@ -1,14 +1,20 @@
 package com.xl.spring.web.controllers;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class HomeController {
 
+	private static Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping("/")
 	public String showHome(Model model) {
+		logger.debug("showing home page");
 		return "home";
 	}
 }
